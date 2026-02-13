582bc9dc329112171fcf11170676567239e7821d
Heron Mia <howa.anil@gmail.com>
2026-02-13 21:46:18 +0600

    js

--- src/components/Welcome/WelcomePage.jsx
+++ src/components/Welcome/WelcomePage.jsx
@@ -4,10 +4,10 @@ const WelcomePage = () => {
   return (
     <section className=" bg-gradient-to-b from-gray-100 to-gray-400 ">
       <div className="h-screen  flex flex-col items-center justify-center">
-        <h1 className="text-blue-950 text-6xl font-bold ">
+        <h1 className="text-blue-950 text-6xl font-bold text-center">
           Welcome to Ecomdrive
         </h1>
-        <h3 className="pt-4 pb-8 text-blue-500 text-xl font-medium">
+        <h3 className="pt-4 pb-8 text-blue-500 text-xl text-center font-medium">
           Automate and streamline
         </h3>
         <button className="p-4 bg-blue-700 text-white rounded-lg font-medium text-lg">
